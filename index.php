40e8e2df5d6540b34c5f3f7095ad23b75182c0cd
AdrienGCL <adriengicquel.3d@gmail.com>
2025-08-12 15:54:45 +0200

    Page d'accueil (mobile)

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
-    <div class="globlaContainer container-fluid vh-100 d-flex flex-column m-0 p-0 secondaryBgColor">
+    <div class="globlaContainer d-flex flex-column m-0 p-0 secondaryBgColor">
         <?php require_once "html/header.html"; ?>
 
-        <main class="mainContainer d-flex flex-column col container-fluid align-items-center justify-content-center">
-            <article class="presentation row align-items-center justify-content-center mainBgColor">
+        <main class="mainContainer d-flex flex-column col align-items-center justify-content-center">
+            <article class="presentation m-0 row align-items-center justify-content-center mainBgColor">
                 <div class="presTitle row">
                     <img class="logoMini" src="assets/images/logo_ecoride_mini.jpg" alt="Logo de la société EcoRide(">
                 </div>
-                <div class="row padding-10 align-items-center justify-content-center">
-                    <div class="col-md-auto padding-5">
+                <div class="row m-0 align-items-center justify-content-center">
+                    <div class="col-md-auto col m-0 padding-5">
                         <div class="stackParent position-relative round-10 overflow-hidden">
                             <img class="imgAccueil img-fluid" src="assets/images/AdobeStock_696167858.jpg" alt="">
                             <div class="position-absolute top-50 start-50 translate-middle">
@@ -28,7 +28,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-auto padding-5">
+                    <div class="col-md-auto col m-0 padding-5">
                         <div class="stackParent position-relative round-10 overflow-hidden">
                             <img class="imgAccueil img-fluid" src="assets/images/AdobeStock_292730611.jpg" alt="">
                             <div class="position-absolute top-50 start-50 translate-middle">
@@ -36,7 +36,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-auto padding-5">
+                    <div class="col-md-auto col m-0 padding-5">
                         <div class="stackParent position-relative round-10 overflow-hidden">
                             <img class="imgAccueil img-fluid" src="assets/images/AdobeStock_233664001.jpg" alt="">
                             <div class="position-absolute top-50 start-50 translate-middle">
